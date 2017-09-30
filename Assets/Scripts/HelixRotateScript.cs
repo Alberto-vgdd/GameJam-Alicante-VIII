8ca80f8ef0b0f3979cdb0452dfd46dd8ddb09d32
Jonatan <al315616@uji.es>
2017-09-30 20:12:52 +0200

    Level 1 complete playable
    Playable complete level 1

---
+++ Assets/Scripts/HelixRotateScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixRotateScript : MonoBehaviour {
+
+
+    float rotationZ = 325f;
+    public Transform helix;
+
+    [Header("Click Position Script")]
+    public ClickPositionScript m_ClickPositionScript;
+
+
+    private bool m_HelixActive;
+    [Header("Parametres Helix")]
+
+    public float zspeedRotation;
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+        if (m_ClickPositionScript.HasBeenClicked())
+        {
+
+            m_HelixActive = true;
+            
+           
+        }
+
+        if (m_HelixActive)
+        {
+            rotationZ += zspeedRotation;
+            helix.localEulerAngles = new Vector3(0,0,rotationZ);
+
+        }
+    }
+}
