4be98a9fdd0659d86879f50011f305cb479717ca
Albert Dols Ferrandis <al315151@uji.es>
2017-09-30 19:08:05 +0200

    Ajustado Parallax y sus imagenes
    Tambien esta el nuevo taca y nivel UnoDos

---
+++ Assets/Scripts/ParallaxBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBehaviour : MonoBehaviour {
+
+	GameObject player;
+
+	public GameObject[] parallaxObjects;
+
+	// Use this for initialization
+	void Start () {
+		player =  GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		parallaxObjects [0].transform.position = new Vector3 (player.transform.position.x * 0.2f, 0f, 30f);
+		parallaxObjects [1].transform.position = new Vector3 (player.transform.position.x * 0.5f, 0f, 30f);
+		
+	}
+}
