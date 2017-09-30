a22ed62c7b0b3c57f53ac5b78d4f1fa52da53c00
Alberto-vgdd <alberto.vgdd@gmail.com>
2017-09-30 13:13:17 +0200

    Player Movement

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -8,39 +8,78 @@ public class PlayerMovementScript : MonoBehaviour
 	public Transform m_PlayerTransform;
 	public Rigidbody2D m_PlayerRigidbody2D;
 	public CapsuleCollider2D m_PlayerCapsuleCollider2D;
+
+	[Header("Ball Components")]
+	public Transform m_BallTransform;
+	public Rigidbody2D m_BallRigidbody2D;
 	
 	[Header("Scene Camera")]
 	public Camera m_SceneCamera;
 
 	[Header("Player Movement Parameters")]
 	public float m_MovementSpeed;
-	public float m_RunMultiplier;
+	public float m_FastMovementpeeed;
+	public float m_JumpSpeed;
 	public float m_RayToGroundDistance;
 	public float m_MaxSlopeAngle;
+
+	[Header("Ball Movement Parameters")]
+	public float m_BallSmooth;
+	public float m_MaximumBallDistance;
+	
 
 
 
 	// Player Movement Inputs
 	private float m_HorizontalInput;
+	private bool m_JumpInput;
+	private Vector3 m_MousePositionInWorld;
+	private bool m_ChangeState;
 
 	// Player Grounded variables
-	public bool m_PlayerGrounded;
-	public bool m_PlayerSliding;
+	private bool m_PlayerGrounded;
+	private bool m_PlayerSliding;
 	RaycastHit2D[] m_RaycastHit2DArray;
 	private Vector2 m_FloorNormal;
 
 
+	// Ball Movement Variables
+	private Vector2 m_TargetBallPosition;
+	private Vector2 m_CurrentBallVelocity;
+
+	// Player & Ball States
+	private bool m_PlayerBallTogether;
+	private bool m_PlayerBallLinked;
+	private bool m_PlayerOnly;
+
 	void Start () 
 	{
+		m_PlayerOnly = false;
+		m_PlayerBallLinked = true;
+		m_PlayerBallTogether = false;
 	}
 	
 	void Update () 
 	{
 		// Update player Inputs
 		m_HorizontalInput = Input.GetAxisRaw("Horizontal");
+		m_JumpInput = Input.GetButton("Jump");
+		m_MousePositionInWorld = m_SceneCamera.ScreenToWorldPoint(Input.mousePosition+Vector3.forward*m_SceneCamera.nearClipPlane);
+		m_ChangeState = Input.GetMouseButtonDown(1);
+
+
+		// Change Player and Ball States
+		ChangeState();
 	}
 
 	void FixedUpdate()
+	{
+		MovePlayer();
+		MoveBall();
+	}
+
+
+	void MovePlayer()
 	{
 		// Cast a capsule below the player
 		m_RaycastHit2DArray = Physics2D.CapsuleCastAll(new Vector2(m_PlayerTransform.position.x,m_PlayerTransform.position.y)+m_PlayerCapsuleCollider2D.offset,m_PlayerCapsuleCollider2D.size,m_PlayerCapsuleCollider2D.direction,m_PlayerTransform.eulerAngles.z,-Vector2.up,m_RayToGroundDistance, (1 << LayerMask.NameToLayer("Scenario")));
@@ -74,8 +113,7 @@ public class PlayerMovementScript : MonoBehaviour
 			}
 		}
 		
-
-
+		
 		// Move the player
 		m_PlayerRigidbody2D.velocity = Vector2.right*m_MovementSpeed*m_HorizontalInput + Vector2.up*m_PlayerRigidbody2D.velocity.y;
 		
@@ -84,20 +122,49 @@ public class PlayerMovementScript : MonoBehaviour
 		if (m_PlayerGrounded && m_FloorNormal != Vector2.up) {	m_PlayerRigidbody2D.velocity = Vector3.ProjectOnPlane(m_PlayerRigidbody2D.velocity,m_FloorNormal); }
 		// Apply Sliding to the player
 		if (m_PlayerSliding){ 	m_PlayerRigidbody2D.velocity = new Vector2(m_PlayerRigidbody2D.velocity.x, Mathf.Min(m_PlayerRigidbody2D.velocity.y,-2f*m_MovementSpeed)); 	}
+
+
+		//Manage the jump
+		if (m_PlayerGrounded && m_JumpInput)
+		{
+			if (!m_PlayerSliding)
+			{
+				m_PlayerRigidbody2D.velocity = Vector2.right*m_PlayerRigidbody2D.velocity.x+Vector2.up*m_JumpSpeed;
+			}
+		}
 		
 
 
 		// Finally, add the gravity force
-		m_PlayerRigidbody2D.AddForce(Physics2D.gravity);
+		m_PlayerRigidbody2D.AddForce(Physics2D.gravity*4f);
 	}
 
+	void MoveBall()
+	{
+		if (m_PlayerBallLinked && !m_PlayerBallTogether && !m_PlayerOnly)
+		{
+			float mouseDistance = Mathf.Min( Vector3.Distance(m_MousePositionInWorld,m_PlayerTransform.position), m_MaximumBallDistance);
+			Vector3 directionToMouse = (m_MousePositionInWorld-m_PlayerTransform.position).normalized;
 
-
-
-
-	//Depaluego
-		// m_MousePositionInWorld = m_SceneCamera.ScreenToWorldPoint(Input.mousePosition+Vector3.forward*m_SceneCamera.nearClipPlane);
-		// m_MousePositionInWorld.z = 0;
-
-		// m_BallRigidbodies.MovePosition(Vector3.SmoothDamp(m_BallRigidbodies.position,m_MousePositionInWorld,ref m_BallVelocity,0.05f));
+			m_TargetBallPosition = m_PlayerTransform.position+directionToMouse*mouseDistance;
+		}
+		else
+		{
+			m_TargetBallPosition = m_PlayerTransform.position;
+		}
+		
+		m_BallRigidbody2D.MovePosition(Vector2.SmoothDamp(m_BallRigidbody2D.position,m_TargetBallPosition,ref m_CurrentBallVelocity,m_BallSmooth,100f,Time.fixedDeltaTime));
+		
+	}
+	
+	void ChangeState()
+	{
+		if (m_ChangeState)
+		{
+			m_PlayerBallTogether = !m_PlayerBallTogether;
+			m_PlayerBallLinked = !m_PlayerBallLinked;
+		}
+	}
 }
+
+	
