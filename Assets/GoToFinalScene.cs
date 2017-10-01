6d7fed15a57159e5ed43d8948bf6094c40652f00
Israel Nebot <israel.nebot@gmail.com>
2017-10-01 09:37:53 +0200

    FINIQUITAO FINIQUITAO CASI

--- Assets/GoToFinalScene.cs
+++ Assets/GoToFinalScene.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GoToFinalScene : MonoBehaviour {
 
+	public GameObject m_Canvas;
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void Load(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+	}
 	
-	// Update is called once per frame
-	void Update () {
+	void OnTriggerEnter2D(Collider2D coll){
+
+		if(coll.gameObject.tag == "Player"){
+			m_Canvas.GetComponent<Animation>().Play();
+			Invoke("Load", 2f);	
+		}
 		
 	}
 }
