4bbb3a026b9c3fd64b3a8fe007a367dde9a6e102
Aupuma <aurepuerta@gmail.com>
2017-10-01 02:48:50 +0200

    Añadida planta disparadora
    Se ha incluido una platnta que lanza proyectiles que salen disparados
    inicialmente hacia el jugador

---
+++ Assets/PlantShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantShooting : MonoBehaviour {
+
+    public float shootingInterval;
+    public GameObject toxicOrb;
+    public Transform shootingPos;
+
+    Animation shootingAnimation;
+    float secondsToShoot;
+    
+	// Use this for initialization
+	void Start () {
+        secondsToShoot = shootingInterval;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (secondsToShoot > 0) secondsToShoot -= Time.deltaTime;
+        else
+        {
+            Instantiate(toxicOrb, shootingPos.position,shootingPos.rotation);
+            secondsToShoot = shootingInterval;
+        }
+	}
+}
---
+++ Assets/ToxicShot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToxicShot : MonoBehaviour {
+
+    public float speed;
+    GameObject player;
+    Vector3 dirToPlayer;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerCenter = new Vector3(player.transform.position.x + 1, 2f, player.transform.position.y + 1.6f);
+        dirToPlayer = playerCenter - this.transform.position;
+    }
+
+    private void Update()
+    {
+        this.transform.Translate(dirToPlayer.normalized * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Ball" || coll.gameObject.tag == "Player" || coll.gameObject.layer == LayerMask.NameToLayer("Scenario"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
